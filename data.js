de144b7d0a54b4a07dbc1dc66f65b17e1e5a3634
rogelio1502 <rogelio15021515@gmail.com>
2023-01-23 14:44:41 -0600

    new data

--- data.js
+++ data.js
@@ -6,23 +6,23 @@ module.exports = {
     },
     {
       value: '18203939',
-      text: 'fsfdaasfds'
+      text: 'Javier'
     },
     {
       value: '18209349',
-      text: 'fsfdfdfds'
+      text: 'Fernando'
     },
     {
       value: '18209394',
-      text: 'fsfddfdsfds'
+      text: 'Anabolico'
     },
     {
       value: '1820931',
-      text: 'fsfdsfhhds'
+      text: 'Celular'
     },
     {
       value: '182093904',
-      text: 'fsffdgdsfds'
+      text: 'Plancton'
     }
   ]
 }
