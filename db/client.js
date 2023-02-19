0d093d61e8ebb3db6b7f03d0925353d2ba54f455
rogelio1502 <rogelio15021515@gmail.com>
2023-02-19 16:14:48 -0600

    added moongose models

--- db/client.js
+++ db/client.js
@@ -1,18 +1,43 @@
 const mongoose = require('mongoose')
-const log_ = mongoose.model('LOG_', {
+const Promedica = mongoose.model('Promedica', {
   url: String,
   org: String,
   exc_message: String,
   status: String
 })
 
+const Paramount = mongoose.model('Paramount', {
+  url: String,
+  org: String,
+  exc_message: String,
+  status: String
+})
+
+const SeniorCare = mongoose.model('SeniorCare', {
+  url: String,
+  org: String,
+  exc_message: String,
+  status: String
+})
 const uri =
   'mongodb+srv://rogelio15:susana15@cluster0.uuakb.mongodb.net/?retryWrites=true&w=majority'
 const run = (url, org, exc_message, status) => {
   mongoose.connect(uri)
+  let o
+  switch (org.toLowerCase()) {
+    case 'promedica':
+      o = new Promedica({ url, org, exc_message, status })
+      return o.save()
 
-  const new_log_ = new log_({ url, org, exc_message, status })
-  return new_log_.save()
+    case 'paramount':
+      o = new Paramount({ url, org, exc_message, status })
+      return o.save()
+    case 'seniorcare':
+      o = new SeniorCare({ url, org, exc_message, status })
+      return o.save()
+    default:
+      break
+  }
 }
 
 module.exports = { run }
